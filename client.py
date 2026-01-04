f09c315130214d98384c3e7ea096559bb2f485fb
wyb51 <2411805122@qq.com>
2026-01-05 00:22:59 +0800

    client.py

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import socket
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = socket.gethostname()
-port = 9999
+if len(sys.argv) != 3:
+    print("Usage: python client.py [host] [port]")
+    exit(1)
+
+host = sys.argv[1]
+port = int(sys.argv[2])
 
 s.connect((host, port))
 msg = s.recv(1024)
